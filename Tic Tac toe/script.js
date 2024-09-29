916ba8a5372efde5bd8abd7ad71bc2f0b83fa36d
kuntal545 <kuntalbej@gmail.com>
2024-09-29 21:34:52 +0530

    add aimode

--- Tic Tac toe/script.js
+++ Tic Tac toe/script.js
@@ -1,10 +1,11 @@
-console.log("Welcome to Tic Tac Toe")
+console.log("Welcome to Tic Tac Toe");
 let music = new Audio("background.mp3");
 let audioTurn = new Audio("ting.mp3");
 let win = new Audio("win.mp3");
 let drawSound = new Audio("a.mp3"); // Define your draw sound
 let turn = "X";
 let isgameover = false;
+let isAiMode = false;  // Flag to indicate if AI mode is active
 
 // Function to change the turn
 const changeTurn = () => {
@@ -41,8 +42,6 @@ const checkWin = () => {
 
             // Show win animation
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-           
-            
         }
     });
 
@@ -61,7 +60,31 @@ const checkWin = () => {
             music.pause(); // Stop the background music
             drawSound.play(); // Play draw sound
             document.getElementById('drawGif').style.width = "200px";
-          
+        }
+    }
+}
+
+// AI move logic
+const aiMove = () => {
+    let emptyBoxes = [];
+    let boxtext = document.getElementsByClassName('boxtext');
+    
+    // Collect empty boxes
+    Array.from(boxtext).forEach((element, index) => {
+        if (element.innerText === "") {
+            emptyBoxes.push(index);
+        }
+    });
+
+    // Choose a random empty box for AI's move
+    if (emptyBoxes.length > 0) {
+        let randomBoxIndex = emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
+        boxtext[randomBoxIndex].innerText = turn;
+        turn = changeTurn();
+        audioTurn.play();
+        checkWin();
+        if (!isgameover) {
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
         }
     }
 }
@@ -79,19 +102,23 @@ Array.from(boxes).forEach(element => {
             checkWin();
             if (!isgameover) {
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+                if (isAiMode && turn === "0") {
+                    setTimeout(aiMove, 500); // AI takes a turn with some delay
+                }
             } 
         }
     });
 });
 
 // Add onclick listener to reset button
-reset.addEventListener('click', () => {
+document.getElementById('reset').addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
         element.innerText = "";
     });
     turn = "X"; 
     isgameover = false;
+    isAiMode = false;  // Reset to two-player mode
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
     document.getElementById('drawGif').style.width = "0px";
@@ -104,4 +131,16 @@ reset.addEventListener('click', () => {
     drawSound.pause();
     drawSound.currentTime = 0;
 });
+
+
+// Add listener to AI mode button
+document.getElementById('aiMode').addEventListener('click', () => {
+    isAiMode = true;
+    document.getElementsByClassName("info")[0].innerText = "AI Activated! Turn for X";
+    turn = "X";  // Reset turn to X
+    isgameover = false;
+});
+
+
+
 
